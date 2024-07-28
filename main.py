d3088783861f75dbb69a64fc5013600e27e1eece
shin5ok <shin5ok@55mp.com>
2024-07-28 13:57:59 +0900

    add capability to select llm from some options

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import io
 import os
+from pprint import pprint as pp
 
 from base64 import b64encode
 from operator import itemgetter
@@ -14,11 +15,39 @@ from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
 from langchain.schema.runnable.config import RunnableConfig
 from langchain_core.messages import HumanMessage
 from langchain_google_vertexai.model_garden import ChatAnthropicVertex
+from langchain_google_vertexai import ChatVertexAI
 
 PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT")
 LOCATION = "europe-west1"
 
-model = "claude-3-5-sonnet@20240620"
+default_model = "Gemini-1.5-Flash"
+
+models = {
+    "Gemini-1.5-Flash": {
+        "model":"gemini-1.5-flash-001",
+        "description": "Gemini 1.5 Flash",
+        "icon": "https://picsum.photos/300",
+    },
+    "Claude-3.5-sonnet": {
+        "model": "claude-3-5-sonnet@20240620",
+        "description": "Claude 3.5 Sonnet",
+        "icon": "https://picsum.photos/390",
+    },
+}
+
+@cl.set_chat_profiles
+async def chat_profile():
+    profiles = []
+    for key, value in models.items():
+        profiles.append(
+            cl.ChatProfile(
+                name=key,
+                markdown_description=value["description"],
+                icon=value["icon"],
+            )
+        )
+    pp(profiles)
+    return profiles
 
 @cl.on_chat_start
 async def main():
@@ -27,24 +56,24 @@ async def main():
             Select(
                 id="Model",
                 label="Vertex AI - Model",
-                values=[model],
+                values= models.keys(),
                 initial_index=0,
-            ),
-            Slider(
-                id="Temperature",
-                label="Temperature",
-                initial=0.3,
-                min=0,
-                max=1,
-                step=0.1,
             ),
             Slider(
                 id="MAX_TOKEN_SIZE",
                 label="Max Token Size",
                 initial=1024,
-                min=256,
-                max=8192,
-                step=256,
+                min=1024,
+                max=0,
+                step=512,
+            ),
+            Slider(
+                id="Temperature",
+                label="Temperature",
+                initial=0.6,
+                min=0,
+                max=1,
+                step=0.1,
             ),
         ]
     ).send()
@@ -52,23 +81,40 @@ async def main():
 
 @cl.on_settings_update
 async def setup_runnable(settings):
+
     cl.user_session.set(
         "memory", ConversationBufferMemory(return_messages=True)
     )
 
     memory = cl.user_session.get("memory")
 
-    llm = ChatAnthropicVertex(
-        model_name=model,
-        project=PROJECT_ID,
-        location=LOCATION,
-        temperature=settings["Temperature"],
-        max_output_tokens=settings["MAX_TOKEN_SIZE"],
-    )
+    chat_profile = cl.user_session.get("chat_profile")
+
+    pp(settings)
+    pp(chat_profile)
+
+    if settings["Model"] != default_model and chat_profile != default_model:
+
+        llm = ChatVertexAI(
+            model_name=models["Gemini-1.5-Flash"]["model"],
+            project=PROJECT_ID,
+            location=LOCATION,
+            temperature=settings["Temperature"],
+            max_output_tokens=settings["MAX_TOKEN_SIZE"],
+        )
+    else:
+
+        llm = ChatAnthropicVertex(
+            model_name=models["Claude-3.5-sonnet"]["model"],
+            project=PROJECT_ID,
+            location=LOCATION,
+            temperature=settings["Temperature"],
+            max_output_tokens=settings["MAX_TOKEN_SIZE"],
+        )
 
     prompt = ChatPromptTemplate.from_messages(
         [
-            ("system", "You are a helpful chatbot"),
+            ("system", "You are the smartest chat bot"),
             MessagesPlaceholder(variable_name="history"),
             MessagesPlaceholder(variable_name="human_message")
         ]
@@ -77,10 +123,7 @@ async def setup_runnable(settings):
     runnable = (
         RunnablePassthrough.assign(
             history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
-        )
-        | prompt
-        | llm
-        | StrOutputParser()
+        ) | prompt | llm | StrOutputParser()
     )
     cl.user_session.set("runnable", runnable)
 
