3fb57944fb36799c2e2ce6ca61aec5c9e3deee2a
shin5ok <shin5ok@55mp.com>
2025-05-31 13:18:56 +0900

    modify order of option of models so chat Claude-4-sonnet moves to the first

--- models_config.py
+++ models_config.py
@@ -2,6 +2,13 @@ from langchain_google_vertexai.model_garden import ChatAnthropicVertex
 from langchain_google_vertexai import ChatVertexAI
 
 models = {
+    "Claude-4-sonnet": {
+        "model": "claude-sonnet-4@20250514",
+        "description": "Claude Sonnet 4",
+        "icon": "https://picsum.photos/390",
+        "class": ChatAnthropicVertex,
+        "region": "us-east5",
+    },
     "Gemini-2.5-Flash": {
         "model": "gemini-2.5-flash-preview-05-20",
         "description": "Gemini 2.5 Flash (Preview)",
@@ -15,12 +22,5 @@ models = {
         "icon": "https://picsum.photos/300",
         "class": ChatVertexAI,
         "region": "us-central1",
-    },
-    "Claude-4-sonnet": {
-        "model": "claude-sonnet-4@20250514",
-        "description": "Claude Sonnet 4",
-        "icon": "https://picsum.photos/390",
-        "class": ChatAnthropicVertex,
-        "region": "us-east5",
     },
 }
